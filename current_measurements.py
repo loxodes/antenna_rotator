1d8c606c1034585c2d294d2a717f9324c896977e
Jon Klein <jtklein@alaska.edu>
2013-05-23 18:01:48 -0800

    added (untested) functions to measure current from current sense amplifier connected to scope, may need to fix triggering

---
+++ current_measurements.py
@@ -0,0 +1,23 @@
+# jon klein, jtklein@alaska.edu
+# mit license
+# functions to automate current measurements
+
+from pylab import *
+from scope_control import *
+
+CURRENT_MEASTIME = 100 # milliseconds
+CURRENT_MEASDELAY = 0 # milliseconds
+CURRENT_CHANNEL = 1
+CURRENT_POINTS = 500
+TRANSCONDUCTANCE_GAIN = 50 / .02 
+
+def measure_avgcurrent(scope, current_scale = TRANSCONDUCTANCE_GAIN, channel = CURRENT_CHANNEL, time = CURRENT_MEASTIME, delay = CURRENT_MEASDELAY):
+    scope_settimebase(scope, time, delay)
+    avg_voltage = average(scope_get_raw(scope, CURRENT_CHANNEL, CURRENT_POINTS) 
+    return (avg_voltage * IGAIN)
+
+if __name__ == '__main__':
+    scope = scope_init()
+    print measure_avgcurrent(scope)
+    
+    
